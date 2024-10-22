b55c9a4c771f66ead07c1ec1bdc5abbf1c37e45f
Savindu Amalka <savinduamalka@gmail.com>
2024-10-22 12:43:46 +0530

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import Header from "./components/header/header"
+import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom";
+import AdminPage from "./pages/adminPages/admin";
+import HomePage from "./pages/client-pages/homePage";
 
 function App() {
   return (
     <>
-     <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route
+            path="/*"
+            element={<h1>404, Page Not Found</h1>}/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+// 60% - #FEF9F2
+// 30% - #7E60BF
+// 10% - #E4B1F0
