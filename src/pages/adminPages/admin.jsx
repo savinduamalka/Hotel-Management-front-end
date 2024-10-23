388865b8be4f9cb31e3499b1918c4ff1653264f8
savinduamalka <savinduamalka@gmail.com>
2024-10-24 00:09:46 +0530

    add routes to admin components

--- src/pages/adminPages/admin.jsx
+++ src/pages/adminPages/admin.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Link, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Bookings from "./admincomponents/bookings";
+import Categories from "./admincomponents/categories";
+import Rooms from "./admincomponents/rooms";
+import Users from "./admincomponents/users";
+import Feedback from "./admincomponents/feedback";
+import Gallery from "./admincomponents/gallery";
 
 export default function AdminPage() {
   return (
@@ -8,37 +14,37 @@ export default function AdminPage() {
         <div className="mb-8 text-3xl font-bold text-center">Admin Panel</div>
         <div className="flex flex-col space-y-6">
           <Link
-            to="/bookings"
+            to="bookings"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Bookings
           </Link>
           <Link
-            to="/categories"
+            to="categories"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Categories
           </Link>
           <Link
-            to="/rooms"
+            to="rooms"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Rooms
           </Link>
           <Link
-            to="/users"
+            to="users"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Users
           </Link>
           <Link
-            to="/feedback"
+            to="feedback"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Feedback
           </Link>
           <Link
-            to="/gallery"
+            to="gallery"
             className="text-2xl hover:text-[#E4B1F0] transition-colors duration-300"
           >
             Gallery Items
@@ -51,6 +57,15 @@ export default function AdminPage() {
         <p className="mt-4 text-lg text-[#7E60BF]">
           Manage bookings, rooms, categories, and more.
         </p>
+        <hr className="my-8 border-t-2 border-[#7E60BF]" />
+        <Routes>
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
       </div>
     </div>
   );
