5537ecb0226b63965fcad887c2e691558b46a5d7
savinduamalka <savinduamalka@gmail.com>
2024-10-25 09:05:04 +0530

    add image for admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom";
 import AdminPage from "./pages/adminPages/admin";
 import HomePage from "./pages/client-pages/homePage";
+import LoginPage from "./pages/login/loginPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route
             path="/*"
             element={
