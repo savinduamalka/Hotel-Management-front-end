58336d33c71d1a8140ff5de6e5d02f6cd4521d13
savinduamalka <savinduamalka@gmail.com>
2024-10-25 11:01:42 +0530

    set use State to credential

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import './loginPage.css'
+import React, { useState } from "react";
+import "./loginPage.css";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-center bg-cover image">
-      <div className="w-[550px]  bg-white bg-opacity-20 backdrop-blur-lg rounded-lg p-10 shadow-lg">
-        <h1 className="text-4xl font-bold text-center text-[#FEF9F2] mb-8">Login</h1>
+      <div className="w-[450px]  bg-white bg-opacity-20 backdrop-blur-lg rounded-lg p-10 shadow-lg">
+        <h1 className="text-4xl font-bold text-center text-[#FEF9F2] mb-8">
+          Login
+        </h1>
         <form className="flex flex-col items-center space-y-5">
           <input
             type="email"
             placeholder="Email"
-            className="w-full px-4 py-3 text-[#7E60BF] bg-[#FEF9F2] bg-opacity-80 border border-[#E4B1F0] rounded focus:outline-none focus:ring-2 focus:ring-[#7E60BF] focus:border-transparent"
+            className="w-full px-4 py-3 text-[#7E60BF] bg-[#FEF9F2] bg-opacity-80 border border-[#E4B1F0] rounded focus:outline-none focus:ring-2 focus:ring-[#7E60BF] focus:border-transparent" defaultValue={email}
+            onChange={
+                (e) => {
+                    setEmail(e.target.value);
+                }
+            }
           />
           <input
             type="password"
             placeholder="Password"
             className="w-full px-4 py-3 text-[#7E60BF] bg-[#FEF9F2] bg-opacity-80 border border-[#E4B1F0] rounded focus:outline-none focus:ring-2 focus:ring-[#7E60BF] focus:border-transparent"
+            defaultValue={password} onChange={
+                (e)=>{
+                    setPassword(e.target.value);
+                }}
           />
           <button className="w-full py-3 bg-[#7E60BF] text-[#FEF9F2] font-semibold rounded hover:bg-[#6A4FA0] transition duration-300">
             Login
@@ -23,5 +37,5 @@ export default function LoginPage() {
         </form>
       </div>
     </div>
-  )
+  );
 }
