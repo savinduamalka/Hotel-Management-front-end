8846b95be8f90f16c67a9a015aa4f942aa2d6c26
savinduamalka <savinduamalka@gmail.com>
2024-10-26 00:38:59 +0530

    display user name on home page when logged

--- src/components/userData/userData.jsx
+++ src/components/userData/userData.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function UserProfile({ img, name }) {
+function UserProfile(props) {
+  const [name, setName] = useState('');
+  const [isChanged, setIsChanged] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setName('Guest');
+    } else {
+      axios.get("http://localhost:3000/api/users", {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json"
+          },
+        })
+        .then((res) => {
+          console.log(res);  
+          setName(res.data.firstname + ' ' + res.data.lastname);   
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log(err.response);
+        });
+    }
+  }, [isChanged]);
+
   return (
     <div className="flex items-center space-x-4 transition-transform duration-300 ease-in-out cursor-pointer hover:scale-105">
-      <img className="rounded-full w-[80px] h-[80px] border-2 border-[#E4B1F0] shadow-lg" src={img} alt="User" />
+      <img className="rounded-full w-[80px] h-[80px] border-2 border-[#E4B1F0] shadow-lg" src={props.img} alt="User" />
       <h1 className="text-[#FEF9F2] text-[22px] font-semibold">{name}</h1>
+    
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          setIsChanged(!isChanged);
+        }}
+        className="bg-[#7E60BF] text-[#FEF9F2] px-4 py-2 rounded hover:bg-[#6A4FA0] transition duration-300"
+        style={{ fontSize: "18px" }}
+      >
+        Logout
+      </button>
+    
     </div>
   );
 }
