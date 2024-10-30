dbd5ba9f56aff2b5db10aa0867e63e2a51bffbc7
savinduamalka <savinduamalka@gmail.com>
2024-10-30 21:40:39 +0530

    set mapping

--- src/pages/adminPages/admincomponents/rooms.jsx
+++ src/pages/adminPages/admincomponents/rooms.jsx
@@ -2,30 +2,71 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function Rooms() {
-  const [room, setRoom] = useState("");
+  const [room, setRoom] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
 
   const token = localStorage.getItem("token");
 
   useEffect(() => {
-    if (token && isLoaded) {
-      axios.get("http://localhost:3000/api/rooms", {
-        Authorization: "Bearer " + token,
-        "Content-Type": "application/json",
-      }).then(
-        (res)=>{
-          console.log(res.data);
-          setRoom(res.data.rooms);
+    if (token && !isLoaded) {
+      axios
+        .get("http://localhost:3000/api/room/", {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log(res.data.list);
+          setRoom(res.data.list);
           setIsLoaded(true);
-        }
-      )
-      .catch(
-        (err)=>{
+        })
+        .catch((err) => {
           console.log(err);
-        }
-      );
+        });
     }
-  }, [isLoaded]);
+  }, [isLoaded, token]);
 
-  return <div></div>;
+  if (!token) {
+    return (
+      <a
+        href="/login"
+        className="bg-[#7E60BF] text-[#FEF9F2] px-4 py-2 rounded hover:bg-[#6A4FA0] transition duration-300"
+        style={{ fontSize: "18px" }}
+      >
+        Login
+      </a>
+    );
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Room Id</th>
+            <th>Category</th>
+            <th>Max Guest</th>
+            <th>Available</th>
+            <th>Images</th>
+            <th>Description</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {room.map((room, index) => (
+            <tr key={index}>
+              <td>{room.RoomId}</td>
+              <td>{room.category}</td>
+              <td>{room.maxGuest}</td>
+              <td>{room.available ? "Yes" : "No"}</td>
+              <td>{room.images}</td>
+              <td>{room.description}</td>
+              <td>{room.notes}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
