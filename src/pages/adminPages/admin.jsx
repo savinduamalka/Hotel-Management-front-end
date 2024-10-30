e6c114a0d021ccabb459bc686ebe1c9898bb9ab6
savinduamalka <savinduamalka@gmail.com>
2024-10-30 21:53:50 +0530

    postion fixed of the admin side bar

--- src/pages/adminPages/admin.jsx
+++ src/pages/adminPages/admin.jsx
@@ -11,7 +11,8 @@ import { FaBookmark, FaClipboardList, FaBed, FaUsers, FaComments, FaImage } from
 export default function AdminPage() {
   return (
     <div className="flex">
-      <div className="w-[30vh] h-[100vh] bg-[#7E60BF] text-[#FEF9F2] flex flex-col py-10 px-4 shadow-lg">
+      
+      <div className="w-[30vh] h-[100vh] bg-[#7E60BF] text-[#FEF9F2] flex flex-col py-10 px-4 shadow-lg fixed">
         <div className="mb-8 text-3xl font-bold text-center">Admin Panel</div>
         <div className="flex flex-col space-y-6">
           <Link
@@ -59,7 +60,7 @@ export default function AdminPage() {
         </div>
       </div>
 
-      <div className="flex-1 bg-[#FEF9F2] p-10">
+      <div className="flex-1 bg-[#FEF9F2] p-10 ml-[30vh]">
         <h1 className="text-4xl font-bold text-[#7E60BF]">Admin Dashboard</h1>
         <p className="mt-4 text-lg text-[#7E60BF]">
           Manage bookings, rooms, categories, and more.
