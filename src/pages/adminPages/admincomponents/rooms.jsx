07e01862211ed1549370f419352b83db9b90fd32
savinduamalka <savinduamalka@gmail.com>
2024-10-30 21:19:24 +0530

    get rooms function done

--- src/pages/adminPages/admincomponents/rooms.jsx
+++ src/pages/adminPages/admincomponents/rooms.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Rooms() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [room, setRoom] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token && isLoaded) {
+      axios.get("http://localhost:3000/api/rooms", {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      }).then(
+        (res)=>{
+          console.log(res.data);
+          setRoom(res.data.rooms);
+          setIsLoaded(true);
+        }
+      )
+      .catch(
+        (err)=>{
+          console.log(err);
+        }
+      );
+    }
+  }, [isLoaded]);
+
+  return <div></div>;
 }
