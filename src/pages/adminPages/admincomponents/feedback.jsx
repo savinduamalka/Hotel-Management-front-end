2c486d090291aa771401b39adb887c1ba3079bcf
savinduamalka <savinduamalka@gmail.com>
2024-11-01 22:20:02 +0530

    getting feedback from database

--- src/pages/adminPages/admincomponents/feedback.jsx
+++ src/pages/adminPages/admincomponents/feedback.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 export default function Feedback() {
+  const [feedback, setFeedback] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const token=localStorage.getItem("token");
+
+  useEffect(
+    () => {
+    if (token && !isLoaded) {
+      axios.get('http://localhost:3000/api/feedback',
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          setCategories(res.data.feedback);
+          setIsLoaded(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [isLoaded]);
+
   return (
     <div>
-      
     </div>
   )
 }
