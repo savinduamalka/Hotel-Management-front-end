44dca401a1220263031f8b7951d671fd5e1c3b37
savinduamalka <savinduamalka@gmail.com>
2024-11-05 23:01:19 +0530

    define use states

--- src/pages/adminPages/admincomponents/categoryFolder/addCategory.jsx
+++ src/pages/adminPages/admincomponents/categoryFolder/addCategory.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function AddCategory() {
+  const [name,setName]=useState('');
+  const[price,setPrice]=useState(0);
+  const [features,setFeatures]=useState([]);
+  const [image,setImages]=useState('');
+  const [isLoaded,setIsLoaded]=useState(false);
+
+  const token = localStorage.getItem("token");
+
+  if(!token){
+    window.location.href="/login";
+  }
+
+
+
+
   return (
     <div>
-      Test
+      
+      
     </div>
   )
 }
