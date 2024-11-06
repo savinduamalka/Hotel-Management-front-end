8cbea139d75b093593a9f6088347e8460cc6de2d
savinduamalka <savinduamalka@gmail.com>
2024-11-06 22:09:28 +0530

    try to debug the errors

--- src/utiles/mediaUpload.js
+++ src/utiles/mediaUpload.js
@@ -1,21 +1,20 @@
 import app from "../config/firebase";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
-const storage = getStorage(app, import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+const storage = getStorage(app); // Removed storage bucket argument
 
 export default function uploadMedia(file) {
   if (!file) {
     console.log("No file selected.");
-    return;
+    return Promise.reject("No file selected");
   }
 
   const fileRef = ref(storage, file.name);
 
-  uploadBytes(fileRef, file)
-    .then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url)=>{
-        console.log(url)
-      })
-
-    })
+  return uploadBytes(fileRef, file)
+    .then((snapshot) => getDownloadURL(snapshot.ref))
+    .catch((error) => {
+      console.error("Error uploading file:", error);
+      throw error;
+    });
 }
