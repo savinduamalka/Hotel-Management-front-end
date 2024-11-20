ecac90a8f04d04ee212d7a4f111b808b09a7f284
savinduamalka <savinduamalka@gmail.com>
2024-11-20 23:56:22 +0530

    combine header with nav bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import UserProfile from "../userData/userData";
 
 function Header() {
   return (
-    <header className="w-full bg-gradient-to-r from-[#7E60BF] to-[#E4B1F0] h-[150px] flex items-center justify-between px-[50px] shadow-2xl relative">
+    <header className="w-full bg-gradient-to-r from-[#7E60BF] to-[#E4B1F0] h-[150px] flex items-center justify-between px-[50px] shadow-none relative top-0">
       <div className="flex-1">
         <h1 className="text-[#FEF9F2] text-[42px] font-extrabold tracking-wider leading-tight">
           Blue Horizon - Galle
