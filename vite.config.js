bcd49c6017fa7b80877844bbc5e27a43705758a3
savinduamalka <savinduamalka@gmail.com>
2025-05-11 00:23:38 +0530

    update dependencies and enhance styling with CSS variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Assuming you're using React
+import path from "path" // Import the 'path' module
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Or your framework's plugin
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Add this line
+      // If your src folder is, for example, 'client/src', use:
+      // "@": path.resolve(__dirname, "./client/src"),
+    },
+  },
 })
