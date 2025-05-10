7d3ca4b934c5d2aeb298ad9b5253a10d604901ed
savinduamalka <savinduamalka@gmail.com>
2025-05-11 00:46:01 +0530

    fix import path for Skeleton component in UserProfile

--- src/components/userData/userData.jsx
+++ src/components/userData/userData.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Skeleton } from "@/ui/skeleton";
+import { Skeleton } from "../ui/skeleton";
 
 // Helper to decode JWT
 function decodeJWT(token) {
