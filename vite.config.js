7ae495532218ad444873690d3c084b455003bad4
Savindu Amalka <savinduamalka@gmail.com>
2025-09-28 20:15:58 +0530

    fix: update axios baseURL to use a static path and refactor vite config for environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react' // Assuming you're using React
 import path from "path" // Import the 'path' module
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], // Or your framework's plugin
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // Add this line
-      // If your src folder is, for example, 'client/src', use:
-      // "@": path.resolve(__dirname, "./client/src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+    plugins: [react()], // Or your framework's plugin
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"), // Add this line
+        // If your src folder is, for example, 'client/src', use:
+        // "@": path.resolve(__dirname, "./client/src"),
+      },
+    },
+  }
 })
